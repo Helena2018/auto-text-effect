e594ade27db5399b42dca44d88d5f03144cce87a
Helena2018 <helena2020liu@gmail.com>
2022-07-14 13:07:06 +1200

    control the speed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const textEl = document.getElementById('text')
 const speedEl = document.getElementById('speed')
-const text = 'We Love Programming !'
+const text = 'We Love Programming ! '
 
 let idx = 1
 let speed = 300 / speedEl.value 
@@ -17,3 +17,5 @@ function writeText() {
 
   setTimeout(writeText, speed)
 }
+
+speedEl.addEventListener('input', (e) => speed = 300 / e.target.value)
